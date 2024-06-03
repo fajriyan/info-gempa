ac665275697d3699407056da12d9304a25eab450
fajriyan20 <fajriyan.sekawanstudio@gmail.com>
2024-06-03 20:21:12 +0700

    update menu mobile

---
+++ src/pages/documentation-api/index.jsx
@@ -0,0 +1,13 @@
+const index = () => {
+  return (
+    <div>
+      <h1>Landing Page Documentation API</h1>
+      <p>
+        Halaman ini akan berisi dokumentasi API secara lengkap yang digunakan
+        pada website ini,{" "}
+      </p>
+    </div>
+  );
+};
+
+export default index;
